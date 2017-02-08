8054c2f56c79cd430b88fca8f7c3d50d818d3e2c
DickyQie <18523978916@163.com>
2017-02-08 10:13:24 +0800

    The use of Android ToolBar

---
+++ app/src/main/java/com/toolbardemo/MainActivity.java
@@ -0,0 +1,59 @@
+package com.toolbardemo;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // App Logo
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        // Title
+        toolbar.setTitle("标题");
+        setSupportActionBar(toolbar);
+        // Navigation Icon 要設定在 setSupoortActionBar 才有作用
+        // 否則會出現 back bottom
+        toolbar.setNavigationIcon(R.drawable.ab_android);
+        // Menu item click 的監聽事件一樣要設定在 setSupportActionBar 才有作用
+        toolbar.setOnMenuItemClickListener(onMenuItemClick);
+    }
+
+    private Toolbar.OnMenuItemClickListener onMenuItemClick = new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            String msg = "";
+            switch (menuItem.getItemId()) {
+                case R.id.action_edit:
+                    msg += "编辑";
+                    break;
+                case R.id.action_share:
+                    msg += "分享";
+                    break;
+                case R.id.action_settings:
+                    msg += "Setting";
+                    break;
+            }
+
+            if(!msg.equals("")) {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+            return true;
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // 為了讓 Toolbar 的 Menu 有作用，這邊的程式不可以拿掉
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+}
